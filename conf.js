dcdb9cb015172d793f3bb8b363fd4509c094e873
louri <louri>
2017-04-07 14:43:43 +0200

    changement du port

--- conf.js
+++ conf.js
@@ -1,10 +1,10 @@
 // conf.js
 exports.config = {
   framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumAddress: 'http://localhost:4445/wd/hub',
   specs: ['spec.js', 'spec2.js'],
   capabilities: {
-    browserName: 'chrome'
+    browserName: 'firefox'
   },
   jasmineNodeOpts: {
 	showColors: true,
