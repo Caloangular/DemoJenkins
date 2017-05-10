21700b416a31eae03afb1dc0d482a2e05057bb28
Caloangular <caloangular@gmail.com>
2017-05-10 10:38:13 +0200

    Update conf.js

--- src/conf.js
+++ src/conf.js
@@ -2,10 +2,10 @@
 exports.config = {
    	directConnect: true,
 	framework: 'jasmine',
-	chromeDriver: '/home/jenkins/firefox/50.0.2/firefox',
+	chromeDriver: 'Chromedriver-2.6_HOME',
 	specs: ['spec.js', 'spec2.js'],
 	capabilities: {
-		browserName: 'firefox'
+		browserName: 'chrome'
 	},
 	jasmineNodeOpts: {
 		showColors: true,
